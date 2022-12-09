49e3f518f30879eb2b17d507587b7d0e1964da9f
unknown <christopherbonime@gmail.com>
2022-12-09 18:20:23 -0500

    started html framework and simple styling, tested weather api url

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={40aa3e5aaeb88d084ff46beb25e47310}
+
+// var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=new york&appid=40aa3e5aaeb88d084ff46beb25e47310"
+
+// fetch(url)
+// .then(res => res.json())
+// .then(data => console.log(data))
